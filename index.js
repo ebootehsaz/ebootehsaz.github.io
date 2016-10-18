6a16a0ae533bc8119ab90cbfda2b07a9d89434ce
ebootehsaz <ethan_boote@yahoo.com>
2016-10-18 14:57:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
    $('.big').mouseenter(function() {
        $(this).animate({
            height: '+=20px'
+             width: ' +=15px'
        });
    });
    $('.big').mouseleave(function() {
        $(this).animate({
            height: '-=15px'
+          width: ' -=10px'
        }); 
    });
    $('.big').click(function() {
